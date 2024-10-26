5fb97d6b2c6eedc52c44651078159e705e0bd5f4
Hashim khan <125628969+hashimcoder@users.noreply.github.com>
2024-10-26 07:31:12 +0000

    commit2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId  = 1234;
+let emailId = "coder7@gmail.com";
+var city = "Rampur";
+let string2;
+string2 = "hashim khan"
+
+ //  if we declaire vaiables name with name keyword then we can not re declaire it 
+ // avoid to use var keyword instead use let and const for scope purpose
+console.table([accountId,emailId,city,string2]);
