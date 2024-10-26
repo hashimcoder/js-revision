0e46613b0612c3c54b3873758303a17a8dd2cd8b
Hashim khan <125628969+hashimcoder@users.noreply.github.com>
2024-10-26 12:11:35 +0000

    comparision in javascript

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,31 @@
+// console.log(2 == 2)
+// console.log(2 !=1)
+// console.log(2>=1);
+
+/// avoid thes type of conversion  
+console.log(null == 0)
+console.log(null > 0)
+console.log(null >= 0)
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined < 0);
+
+ // strict chekcing  === !==
+   console.log("2" == 2) // true 
+   console.log("2" === 2)   //false
+
+
+  
+   // SUMMARY FOR COMPARISION 
+
+
+//    Operator    	Description	      Example	     Result
+// ==        	Loose equality	     5 == '5'	      true
+// ===     	Strict equality	    5 === '5'	     false
+// !=	       Loose inequality   	5 != '5'	     false
+// !==   	Strict inequality	   5 !== '5'	      true
+// >	   Greater than	              10 > 5	      true
+// >=	   Greater than or equal to	    5 >= 5	      true
+// <	   Less than	                 3 < 8	       true
+// <=	    Less than or equal to	    5 <= 5	        true
